828a47d481167c8efdbc37ec6e4e0ebfce48e6ed
job <hujunjob123@hotmail.com>
2024-02-07 17:25:32 +0800

    change to generateMetadata

--- app/page.tsx
+++ app/page.tsx
@@ -24,18 +24,40 @@ const frameMetadata = getFrameMetadata({
   post_url: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
-export const metadata: Metadata = {
-  title: 'web3.study',
-  description: 'LFG',
-  openGraph: {
-    title: 'web3.study',
-    description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/robot.jpg`],
-  },
-  other: {
-    ...frameMetadata,
-  },
-};
+// export const metadata: Metadata = {
+//   title: 'web3.study',
+//   description: 'LFG',
+//   openGraph: {
+//     title: 'web3.study',
+//     description: 'LFG',
+//     images: [`${NEXT_PUBLIC_URL}/robot.jpg`],
+//   },
+//   other: {
+//     ...frameMetadata,
+//   },
+// };
+
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Eventcaster RSVP test",
+    description: "Test event",
+    openGraph: {
+      title: "Eventcaster RSVP test",
+      images: [`${NEXT_PUBLIC_URL}/api/image`],
+    },
+    other: {
+      "fc:frame": "vNext",
+      "fc:frame:image": `${NEXT_PUBLIC_URL}/api/image`,
+      "fc:frame:button:1": "RobotDAO",
+      "fc:frame:button:2": "Settings",
+      "fc:frame:button:3": "Connect",
+      "fc:frame:button:4": "Mint",
+      "fc:frame:post_url": `${NEXT_PUBLIC_URL}/api/frame`,
+    },
+    metadataBase: new URL(NEXT_PUBLIC_URL ?? ""),
+  };
+}
 
 export default function Page() {
   return (
