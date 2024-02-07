a96ec34cf8234cf67e34462c41d4a27791a78645
job <hujunjob123@hotmail.com>
2024-02-07 16:10:13 +0800

    Update route.ts

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -2,7 +2,7 @@ import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/o
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
 
-async function getResponse(req: NextRequest): Promise<NextResponse> {
+async function getResponse(req: NextRequest): Promise<Response> {
   let accountAddress: string | undefined = '';
   let text: string | undefined = '';
   const apiSecret = process.env.KEY;
@@ -24,16 +24,15 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
   //   );
   // }
 
-  return new NextResponse(`
-  <!DOCTYPE html>
-  <html>
-    <head>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="${NEXT_PUBLIC_URL}/api/frame" />
-    </head>
-    <body>hello world</body>
-  </html>
-`)
+  const svgText = `
+            <svg width="${100}" height="${50}">
+                <style>
+                    .title { fill: #fff; font-size: ${10}px; font-family: Arial; }
+                </style>
+                <rect width="100%" height="100%" fill="#333" />
+                <text x="50%" y="50%" class="title" dominant-baseline="middle" text-anchor="middle">${text}</text>
+            </svg>
+        `;
 
   return new NextResponse(
     getFrameHtmlResponse({
@@ -42,7 +41,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
           label: `🌲 Text: ${text}, isValid:${isValid}, by ${accountAddress}, liked:${message?.liked}`,
         },
       ],
-      image: `${NEXT_PUBLIC_URL}/robot.jpg`,
+      // image: `${NEXT_PUBLIC_URL}/robot.jpg`,
+      image:svgText,
       post_url: `${NEXT_PUBLIC_URL}/api/frame`,
     }),
   );
